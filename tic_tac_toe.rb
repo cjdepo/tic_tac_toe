9d99817c7642572464eb02de72f5795e5e14d3aa
cjdepo <cjdepowell@protonmail.com>
2021-11-04 01:52:02 -0400

    bugfix

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -6,66 +6,60 @@ class Player
   def initialize()
     @@player_count += 1
     @player_id = @@player_count
-    @type = @player_id == 1 ? 'x' : "o"
-
-  end
-
-  def self.get_player_count
-    @@player_count
+    @type = @player_id == 1 ? 'x' : 'o'
   end
 
   def play(board, position)
     board.space(self.type, position - 1)
   end
-
 end
 
 class Game
   attr_reader :spaces
 
-  def initialize()
-    @spaces = [ ' ', ' ', ' ',
-                ' ', ' ', ' ',
-                ' ', ' ', ' ']
+  def initialize
+    @spaces = [' ', ' ', ' ',
+               ' ', ' ', ' ',
+               ' ', ' ', ' ']
   end
-  
-  def clear()
-    @spaces = [ ' ', ' ', ' ',
-                ' ', ' ', ' ',
-                ' ', ' ', ' ']
+
+  def clear
+    @spaces = [' ', ' ', ' ',
+               ' ', ' ', ' ',
+               ' ', ' ', ' ']
   end
-  
+
   def space(move, position)
     @spaces[position] = move
   end
 
   def display()
-    p self.spaces[0..2]
-    p self.spaces[3..5]
-    p self.spaces[6..8]
+    p spaces[0..2]
+    p spaces[3..5]
+    p spaces[6..8]
   end
 
   def win?()
     x_indexes = self.spaces.each_with_index.map { |type, index| type == "x" ? index : nil}.compact
     o_indexes = self.spaces.each_with_index.map { |type, index| type == "o" ? index : nil}.compact
     case
-    when ([1, 2, 3] - x_indexes).empty?,
-         ([4, 5, 6] - x_indexes).empty?,
-         ([7, 8, 9] - x_indexes).empty?,
+    when ([0, 1, 2] - x_indexes).empty?,
+         ([3, 4, 5] - x_indexes).empty?,
+         ([6, 7, 8] - x_indexes).empty?,
+         ([0, 3, 6] - x_indexes).empty?,
          ([1, 4, 7] - x_indexes).empty?,
          ([2, 5, 8] - x_indexes).empty?,
-         ([3, 6, 9] - x_indexes).empty?,
-         ([1, 5, 9] - x_indexes).empty?,
-         ([3, 5, 7] - x_indexes).empty?
+         ([0, 4, 8] - x_indexes).empty?,
+         ([2, 4, 6] - x_indexes).empty?
       puts "x wins"
-    when ([1, 2, 3] - o_indexes).empty?,
-         ([4, 5, 6] - o_indexes).empty?,
-         ([7, 8, 9] - o_indexes).empty?,
+    when ([0, 1, 2] - o_indexes).empty?,
+         ([3, 4, 5] - o_indexes).empty?,
+         ([6, 7, 8] - o_indexes).empty?,
+         ([0, 3, 6] - o_indexes).empty?,
          ([1, 4, 7] - o_indexes).empty?,
          ([2, 5, 8] - o_indexes).empty?,
-         ([3, 6, 9] - o_indexes).empty?,
-         ([1, 5, 9] - o_indexes).empty?,
-         ([3, 5, 7] - o_indexes).empty?
+         ([0, 4, 8] - o_indexes).empty?,
+         ([2, 4, 6] - o_indexes).empty?
       puts "o wins"
     else
       puts "nope"
@@ -79,10 +73,9 @@ player_1 = Player.new()
 player_2 = Player.new()
 player_2.play(board_1, 1)
 player_2.play(board_1, 4)
-player_2.play(board_1, 7)
+player_2.play(board_1, 5)
 puts player_1.player_id
 puts player_2.player_id
 player_3 = Player.new()
-puts Player.get_player_count()
 board_1.display
 board_1.win?
