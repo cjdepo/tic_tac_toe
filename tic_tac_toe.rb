3ceef3741f5b3dc9f4f0fc8b1dc35c221c27e60f
cjdepo <cjdepowell@protonmail.com>
2021-11-04 01:11:23 -0400

    function to check for winner added

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -46,6 +46,30 @@ class Game
   end
 
   def win?()
+    x_indexes = self.spaces.each_with_index.map { |type, index| type == "x" ? index : nil}.compact
+    o_indexes = self.spaces.each_with_index.map { |type, index| type == "o" ? index : nil}.compact
+    case
+    when ([1, 2, 3] - x_indexes).empty?,
+         ([4, 5, 6] - x_indexes).empty?,
+         ([7, 8, 9] - x_indexes).empty?,
+         ([1, 4, 7] - x_indexes).empty?,
+         ([2, 5, 8] - x_indexes).empty?,
+         ([3, 6, 9] - x_indexes).empty?,
+         ([1, 5, 9] - x_indexes).empty?,
+         ([3, 5, 7] - x_indexes).empty?
+      puts "x wins"
+    when ([1, 2, 3] - o_indexes).empty?,
+         ([4, 5, 6] - o_indexes).empty?,
+         ([7, 8, 9] - o_indexes).empty?,
+         ([1, 4, 7] - o_indexes).empty?,
+         ([2, 5, 8] - o_indexes).empty?,
+         ([3, 6, 9] - o_indexes).empty?,
+         ([1, 5, 9] - o_indexes).empty?,
+         ([3, 5, 7] - o_indexes).empty?
+      puts "o wins"
+    else
+      puts "nope"
+    end
   end
 end
 
@@ -53,9 +77,9 @@ board_1 = Game.new()
 
 player_1 = Player.new()
 player_2 = Player.new()
-player_1.play(board_1, 1)
-player_2.play(board_1, 5)
-player_2.play(board_1, 9)
+player_2.play(board_1, 1)
+player_2.play(board_1, 4)
+player_2.play(board_1, 7)
 puts player_1.player_id
 puts player_2.player_id
 player_3 = Player.new()
