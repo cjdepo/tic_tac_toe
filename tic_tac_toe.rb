6a1ae82bf8a6730a0f8661d1a95525ce833c95d1
cjdepo <cjdepowell@protonmail.com>
2021-11-04 18:11:07 -0400

    full functionality

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,3 +1,4 @@
+require 'pry-byebug'
 class Player
   attr_reader :player_id, :type, :board
 
@@ -8,7 +9,6 @@ class Player
     @player_id = @@player_count
     @type = @player_id == 1 ? 'x' : 'o'
     @board = board
-
   end
 
   def play(position)
@@ -25,6 +25,63 @@ class Game
                ' ', ' ', ' ']
   end
 
+  def self.new_game
+    game = Game.new
+    player1 = Player.new(game)
+    player2 = Player.new(game)
+    while true
+      position = game.player_turn(1).to_i
+      player1.play(position)
+      game.display
+      if game.win?
+        puts "Player 1 wins!"
+        break
+      end
+      if game.end?
+        puts "Tie!"
+        break
+      end
+
+      position = game.player_turn(2).to_i
+      player2.play(position)
+      game.display
+      if game.win?
+        puts "Player 2 wins!"
+        break
+      end
+      if game.end?
+        puts "Tie!"
+        break
+      end
+    end
+  end
+  
+  def player_turn(player_id)
+    puts "Player #{player_id} your turn.  Enter a number
+    \ncorresponding to the spaces as follows:
+    \n     1   2   3
+    \n     4   5   6
+    \n     7   8   9
+    \nEnter your number:"
+    while true
+      number = gets.to_i
+      case
+      when ![1, 2, 3, 4, 5, 6, 7, 8, 9].include?(number)
+        self.display
+        puts "You must enter a number (1 through 9):"
+        next
+      when @spaces[number - 1] != " "
+        self.display
+        puts "That space is already taken!"
+        next
+      end 
+      if [1, 2, 3, 4, 5, 6, 7, 8, 9].include?(number) && @spaces[number - 1] == " "
+        return number
+        break
+      end
+    end
+  end
+
   def clear
     @spaces = [' ', ' ', ' ',
                ' ', ' ', ' ',
@@ -36,9 +93,15 @@ class Game
   end
 
   def display
+    puts "\n\n\n\n\n\n"
     p spaces[0..2]
     p spaces[3..5]
     p spaces[6..8]
+    puts "\n\n\n\n\n"
+  end
+
+  def end?
+    !@spaces.include?(' ')
   end
 
   def win?
@@ -52,9 +115,8 @@ class Game
          ([1, 4, 7] - x_indexes).empty?,
          ([2, 5, 8] - x_indexes).empty?,
          ([0, 4, 8] - x_indexes).empty?,
-         ([2, 4, 6] - x_indexes).empty?
-      puts 'x wins'
-    when ([0, 1, 2] - o_indexes).empty?,
+         ([2, 4, 6] - x_indexes).empty?,
+         ([0, 1, 2] - o_indexes).empty?,
          ([3, 4, 5] - o_indexes).empty?,
          ([6, 7, 8] - o_indexes).empty?,
          ([0, 3, 6] - o_indexes).empty?,
@@ -62,37 +124,11 @@ class Game
          ([2, 5, 8] - o_indexes).empty?,
          ([0, 4, 8] - o_indexes).empty?,
          ([2, 4, 6] - o_indexes).empty?
-      puts 'o wins'
+      return true
     else
-      puts 'nope'
+      return false
     end
   end
 end
 
-def new_game
-  game = Game.new
-  player1 = Player.new(game)
-  player2 = Player.new(game)
-  move = player_turn(1).to_i
-  player1.play(move)
-  move = player_turn(2).to_i
-  player2.play(move)
-  move = player_turn(1).to_i
-  player1.play(move)
-  move = player_turn(2).to_i
-  player2.play(move)
-  game.display
-  game.win?
-end
-
-def player_turn(player_id)
-  puts "Player #{player_id} your turn.  Enter a number
-  \ncorresponding to the spaces as follows:
-  \n     1   2   3
-  \n     4   5   6
-  \n     7   8   9
-  \nEnter your number:"
-  gets
-end
-
-new_game
+Game.new_game
